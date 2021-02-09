ce16696fd02c4fb373928d5ca0920b60fbe5dde0
Ericcentrik <enaiman1@gmail.com>
2021-02-09 18:02:09 -0500

    started working on admins being able to approve comments

---
+++ Comments.php
@@ -0,0 +1,156 @@
+<?php 
+ require_once( './Includes/DB.php' );
+ require_once( './Includes/Functions.php' );
+ require_once( './Includes/Sessions.php' ); 
+
+ $_SESSION["TrackingURL"]=$_SERVER["PHP_SELF"];
+Confirm_Login();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <!-- CSS only -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+      crossorigin="anonymous"
+    />
+    <!-- css -->
+    <link rel="stylesheet" href="./Css/styles.css">
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/918054a49e.js" crossorigin="anonymous"></script>
+    <title>Manage Comments</title>
+  </head>
+  <body>
+<!-- Navbar -->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a href="#" class="navbar-brand">Ericcentrik.com</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarcollapseCMS" aria-controls="navbarcollapseCMS" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        <div class="collapse navbar-collapse" id="navbarcollapseCMS">
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+                <a href="MyProfile.php" class="nav-link"><i class="fas fa-user text-success"></i> My Profile</a>
+            </li>
+            <li class="nav-item">
+                <a href="Dashboard.php" class="nav-link">Dashboard</a>
+            </li>
+            <li class="nav-item">
+                <a href="Posts.php" class="nav-link">Post</a>
+            </li>
+            <li class="nav-item">
+                <a href="Categories.php" class="nav-link">Categories</a>
+            </li>
+            <li class="nav-item">
+                <a href="Admins.php" class="nav-link">Manage Admins</a>
+            </li>
+            <li class="nav-item">
+                <a href="Comments.php" class="nav-link">Comments</a>
+            </li>
+            <li class="nav-item">
+                <a href="Blog.php?page=1" class="nav-link">Live Blog</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item"><a href="Logout.php" class="nav-link text-danger"><i class="fas fa-user-times"></i> Logout</a></li>
+        </ul>
+    </div> <!--ending toggle button-->
+    </div>
+</nav> 
+<div style="height:10px; background: #27aee1"></div> <!--end navbar-->
+
+<header class="bg-dark text-white py-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+         <h1 class="title"><i class="fas fa-comments"></i> Manage Comments</h1>
+      </div>
+    </div>
+  </div>
+</header> <!--ending header-->
+
+<!-- Main Area Start -->
+<section class="container py-2 mb-4">
+    <div class="row" style="min-height:30px;">
+        <div class="col-l-12" style="min-height:400px;">
+            <h2>Un-Approved Comments</h2>
+            <table class="table table-striped table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>No. </th>
+                        <th>Date&Time</th>
+                        <th>Name</th>
+                        <th>Comment</th>
+                        <th>Approve</th>
+                        <th>Action</th>
+                        <th>Details</th>
+                        
+                    </tr>
+                </thead>
+
+                <?php 
+                    $ConnectingDB;
+                    $sql = "SELECT * FROM comments WHERE status='OFF' ORDER BY id desc";
+                    $Execute = $ConnectingDB->query($sql);
+                    $SrNo = 0;
+                    while ($DataRows= $Execute->fetch()){
+                        $CommentId = $DataRows["id"];
+                        $DateTimeOfComment = $DataRows["datetime"];
+                        $CommenterName = $DataRows["name"];
+                        $CommentContent = $DataRows["comment"];
+                        $CommentPostId= $DataRows["post_id"];
+                        $SrNo++;
+                    
+                ?>
+                <tbody>
+                    <tr>
+                        <td><?php echo htmlentities($SrNo); ?></td>
+                        <td><?php echo htmlentities($DateTimeOfComment); ?></td>
+                        <td><?php echo htmlentities($CommenterName); ?></td>
+                        <td><?php echo htmlentities($CommentContent); ?></td>
+                        <td><a class="btn btn-success" href="ApproveComment.php?id=<?php echo $CommentId;?>"> Approve </a></td>
+                        <td><a class="btn btn-danger" href="DeleteComment.php?id=<?php echo $CommentId;?>"> Delete </a></td>
+                        <td><a class="btn btn-primary" href="FullPost.php?id=<?php echo $CommentPostId;?>" target="_blank"> Live Preview</a></td>
+                    </tr>
+
+                </tbody>
+                <?php } ?>
+            </table>
+        </div>
+    </div>
+</section>
+<!-- Main Area Ends -->
+<br>
+
+
+<footer class="bg-dark text-white">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <p class="lead text-center">Theme By | <a href="https://ericnaiman.com" style="color: white;  cursor: pointer;">Eric Naiman </a>| <span id="year"></span> &copy; "All rights Reserved"
+        </p>
+      </div> 
+    </div>
+  </div>
+</footer> 
+<div style="height:10px; background: #27aee1"></div> <!--ending footer-->
+
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+      crossorigin="anonymous"
+    ></script>
+  <script>
+    document.querySelector('#year').innerHTML= new Date().getFullYear();  
+  </script>
+  </body>
+</html>
