22cd9485f51ad6a9a0cd5995286576dc43ba2a78
Ericcentrik <enaiman1@gmail.com>
2021-02-09 19:06:17 -0500

    added comment approval and disapproval functionality

--- Comments.php
+++ Comments.php
@@ -81,6 +81,10 @@ Confirm_Login();
 <section class="container py-2 mb-4">
     <div class="row" style="min-height:30px;">
         <div class="col-l-12" style="min-height:400px;">
+        <?php 
+        echo ErrorMessage();
+        echo SuccessMessage();
+        ?>
             <h2>Un-Approved Comments</h2>
             <table class="table table-striped table-hover">
                 <thead class="thead-dark">
@@ -116,14 +120,59 @@ Confirm_Login();
                         <td><?php echo htmlentities($DateTimeOfComment); ?></td>
                         <td><?php echo htmlentities($CommenterName); ?></td>
                         <td><?php echo htmlentities($CommentContent); ?></td>
-                        <td><a class="btn btn-success" href="ApproveComment.php?id=<?php echo $CommentId;?>"> Approve </a></td>
-                        <td><a class="btn btn-danger" href="DeleteComment.php?id=<?php echo $CommentId;?>"> Delete </a></td>
+                        <td><a class="btn btn-success" href="ApproveComments.php?id=<?php echo $CommentId;?>"> Approve </a></td>
+                        <td><a class="btn btn-danger" href="DeleteComments.php?id=<?php echo $CommentId;?>"> Delete </a></td>
                         <td><a class="btn btn-primary" href="FullPost.php?id=<?php echo $CommentPostId;?>" target="_blank"> Live Preview</a></td>
                     </tr>
 
                 </tbody>
                 <?php } ?>
             </table>
+
+            <h2>Approved Comments</h2>
+            <table class="table table-striped table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>No. </th>
+                        <th>Date&Time</th>
+                        <th>Name</th>
+                        <th>Comment</th>
+                        <th>Revert</th>
+                        <th>Action</th>
+                        <th>Details</th>
+                        
+                    </tr>
+                </thead>
+
+                <?php 
+                    $ConnectingDB;
+                    $sql = "SELECT * FROM comments WHERE status='ON' ORDER BY id desc";
+                    $Execute = $ConnectingDB->query($sql);
+                    $SrNo = 0;
+                    while ($DataRows= $Execute->fetch()){
+                        $CommentId = $DataRows["id"];
+                        $DateTimeOfComment = $DataRows["datetime"];
+                        $CommenterName = $DataRows["name"];
+                        $CommentContent = $DataRows["comment"];
+                        $CommentPostId= $DataRows["post_id"];
+                        $SrNo++;
+                    
+                ?>
+                <tbody>
+                    <tr>
+                        <td><?php echo htmlentities($SrNo); ?></td>
+                        <td><?php echo htmlentities($DateTimeOfComment); ?></td>
+                        <td><?php echo htmlentities($CommenterName); ?></td>
+                        <td><?php echo htmlentities($CommentContent); ?></td>
+                        <td><a class="btn btn-warning" href="DisApproveComments.php?id=<?php echo $CommentId;?>"> Dis-Approve </a></td>
+                        <td><a class="btn btn-danger" href="DeleteComments.php?id=<?php echo $CommentId;?>"> Delete </a></td>
+                        <td><a class="btn btn-primary" href="FullPost.php?id=<?php echo $CommentPostId;?>" target="_blank"> Live Preview</a></td>
+                    </tr>
+
+                </tbody>
+                <?php } ?>
+            </table>
+
         </div>
     </div>
 </section>
---
+++ DeleteComments.php
@@ -0,0 +1,24 @@
+<?php
+require_once( './Includes/DB.php' );
+require_once( './Includes/Functions.php' );
+require_once( './Includes/Sessions.php' );
+
+?>
+
+<?php
+if ( isset( $_GET['id'] ) ) {
+    $SearchQueryParameter = $_GET['id'];
+    global $ConnectingDB;
+    $sql = "DELETE FROM comments WHERE id='$SearchQueryParameter'";
+    $Execute = $ConnectingDB->query( $sql );
+    if ( $Execute ) {
+        $_SESSION['SuccessMessage'] = 'Comment Deleted Successfully! ';
+        Redirect_to( 'Comments.php' );
+        // code...
+    } else {
+        $_SESSION['ErrorMessage'] = 'Something Went Wrong. Try Again!';
+        Redirect_to( 'Comments.php' );
+    }
+}
+
+?>
